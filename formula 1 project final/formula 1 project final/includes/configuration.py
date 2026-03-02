40ed63d5c81d5e678a35b6eb3879b847296ff2f0
SANTHOSHKUMAR M <isanthoshm36@gmail.com>
2026-03-02 08:16:44 +0530

    Update configuration.py

--- formula 1 project final/formula 1 project final/includes/configuration.py
+++ formula 1 project final/formula 1 project final/includes/configuration.py
@@ -1,3 +1,31 @@
+import org.apache.spark.sql.SparkSession;
+import org.apache.hadoop.conf.Configuration;
+
+public class SparkConfig {
+
+    // Storage Paths - In Java, we typically use 'final' constants for these
+    public static final String RAW_FOLDER_PATH = "wasbs://raw@formula420.blob.core.windows.net";
+    public static final String PROCESSED_FOLDER_PATH = "wasbs://processed@formula420.blob.core.windows.net";
+    public static final String PRESENTATION_FOLDER_PATH = "wasbs://presentation@formula420.blob.core.windows.net";
+
+    /**
+     * Initializes a SparkSession with Azure Blob Storage credentials.
+     * In a Java project, this centralizes your connection logic.
+     */
+    public static SparkSession getSparkSession() {
+        SparkSession spark = SparkSession.builder()
+                .appName("Formula1DataEngineering")
+                .getOrCreate();
+
+        // Access the underlying Hadoop configuration to set Azure credentials
+        Configuration conf = spark.sparkContext().hadoopConfiguration();
+        conf.set("fs.azure.account.key.formula420.blob.core.windows.net", 
+                 "s3f9bzNCqGGaSwqdpSFoofwcaxncyGe6dxcIFj9u2hrbxyyTx2nzG25kV3JYh+5QLERrIE0ZTDjZ+ASthbsy5w==");
+
+        return spark;
+    }
+}
+
 # Databricks notebook source
 spark.conf.set("fs.azure.account.key.formula420.blob.core.windows.net", "s3f9bzNCqGGaSwqdpSFoofwcaxncyGe6dxcIFj9u2hrbxyyTx2nzG25kV3JYh+5QLERrIE0ZTDjZ+ASthbsy5w==")
 
